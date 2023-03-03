42d3d91dd8e88e02bad70902ae6152015daeebdb
Piotr <piotrpog@protonmail.com>
2023-03-03 02:59:24 +0100

    edit screen

--- src/controllers/CatsController.php
+++ src/controllers/CatsController.php
@@ -57,16 +57,50 @@ class CatsController extends Controller
             }            
         }
 
+        $isNew = is_null($catObject->id);
+
         $context = [
             'catObject' => $catObject,
+            'isNew' => $isNew,
         ];
 
-        $html = $this->renderTemplate(
-            'craft-cats-manager/_cat-edit',            
-            $context,
-            Craft::$app->view::TEMPLATE_MODE_CP,
-        );
-        return $html;
+        // OLD METHOD
+
+        // $html = $this->renderTemplate(
+        //     'craft-cats-manager/_cat-edit',            
+        //     $context,
+        //     Craft::$app->view::TEMPLATE_MODE_CP,
+        // );
+        // return $html;
+
+        // NEW METHOD
+
+        $title = $isNew ? Craft::t('craft-cats-manager','Create new cat') : Craft::t('app','Edit') . ' ' . $catObject->name;
+
+        $screen = $this->asCpScreen()
+            ->title($title)
+            ->addCrumb(Craft::t('craft-cats-manager', 'Cats'), 'cats-manager')
+            ->action('craft-cats-manager/cats/cat-save')
+            ->redirectUrl('cats-manager')
+            ->contentTemplate('craft-cats-manager/_cat-edit-screen', $context);
+
+            $screen->addAltAction(Craft::t('app','Save and continue editing'), [
+                'redirect' => 'cats-manager/{id}',
+                'shortcut' => true,
+                'retainScroll' => true,
+            ]);            
+
+            if(!$isNew){
+                $screen->addAltAction(Craft::t('app','Delete'), [
+                    'action' => 'craft-cats-manager/cats/cat-delete',
+                    'redirect' => 'cats-manager',
+                    'destructive' => true,
+                    'confirm' => Craft::t('app','Are you sure you want to delete “{name}”?', ['name' => $catObject->name]),
+                ]);
+            }
+
+        return $screen;
+
     }
     
     public function actionCatSave()
@@ -92,19 +126,30 @@ class CatsController extends Controller
         $catObject->name = $request->getBodyParam('name');
         $catObject->catFood = $request->getBodyParam('catFood');
         
-        // perform save
-        $success = CraftCatsManager::getInstance()->cats->saveCat($catObject);
+        // OLD METHOD
 
-        if (!$success) {
-            $this->setFailFlash(Craft::t('craft-cats-manager', 'Could not save cat'));
-            Craft::$app->getUrlManager()->setRouteParams([
-                'catObject' => $catObject,
-            ]);
-            return null;
+        // perform save
+        // $success = CraftCatsManager::getInstance()->cats->saveCat($catObject);
+
+        // if (!$success) {
+        //     $this->setFailFlash(Craft::t('craft-cats-manager', 'Could not save cat'));
+        //     Craft::$app->getUrlManager()->setRouteParams([
+        //         'catObject' => $catObject,
+        //     ]);
+        //     return null;
+        // }
+
+        // $session->setNotice(Craft::t('craft-cats-manager', 'Cat saved succesfully'));
+        // return $this->redirectToPostedUrl($catObject);
+
+         // NEW METHOD
+
+        if (!CraftCatsManager::getInstance()->cats->saveCat($catObject)) {
+            return $this->asModelFailure($catObject, Craft::t('craft-cats-manager', 'Could not save cat'), 'cat');
         }
 
-        $session->setNotice(Craft::t('craft-cats-manager', 'Cat saved succesfully'));
-        return $this->redirectToPostedUrl($catObject);
+        return $this->asModelSuccess($catObject, Craft::t('craft-cats-manager', 'Cat saved succesfully'), 'cat');
+
     }
 
 
